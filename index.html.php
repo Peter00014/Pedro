77d8aa0387958bfcef10acfa9f7e87af4265f6b0
Peter00014 <pz53970@gmail.com>
2024-12-04 18:57:06 -0600

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,189 @@
+
+
+
+
+</div>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Clínica Dental</title>
+
+
+
+
+</head>
+
+
+<div class="index-container">
+  <div class="image-info-container">
+    <img class="index-image" src="img\index.png" alt="Descripción de la imagen">
+    <div class="text-info">
+      <!-- Información sobre la foto -->
+      <h2>CLINICA DENTAL</h2>
+      <li></li>
+      <p></p>
+      <h3>BIENVENIDOS A NUESTRA CLINICA SALUD DENTAL</h3>
+      <p></p>
+      <p>En la clínica Sevilla Dental llevamos más de 25 años ofreciendo a todos nuestros pacientes un servicio dental integral individualizado, con expertos profesionales altamente cualificados. Realizando tratamientos dentales que mejoran tanto la salud como la estética dental de nuestros pacientes.</p>
+      <p>¿Listo para sonreír con confianza? </p>
+    </div>
+  </div>
+</div>
+
+<!-- ICONOS -->
+<div class="photo-container">
+  <div class="photo">
+    <img src="img\400_1.png" alt="Descripción de la imagen 1">
+    <a class="button" href=>
+      <p>Nustra forma de trabajar</p>
+    </a>
+  </div>
+  <div class="photo">
+    <img src="img\8-min.png" alt="Descripción de la imagen 2">
+    <a class="button" href="">
+      <p>Donde nos encontramos</p>
+    </a>
+  </div>
+  <div class="photo">
+    <img src="img\7-min.png" alt="Descripción de la imagen 3">
+    <a class="button" href="">
+      <p>Tratamientos dentales</p>
+    </a>
+  </div>
+  <div class="photo">
+    <img src="img\6-min.png" alt="Descripción de la imagen 3">
+    <a class="button" href="./Registrar_Cita.php">
+      <p>Urgencias dentales</p>
+    </a>
+  </div>
+  <!-- Agrega más fotos con títulos y enlaces según sea necesario -->
+</div>
+
+
+<!-- TRATAMIENTOS -->
+
+
+<!-- Nuevo apartado -->
+<div class="section-tratamientos">
+  <h2>Tratamientos dentales que ofrecemos desde nuestras</h2>
+  <p>Clínicas dentales en Sevilla</p>
+
+
+  <!-- Primer conjunto de fotos -->
+  <div class="photo-container-tratamientos">
+    <div class="photo-tratamientos">
+      <img src="img\UNO.webp" alt="Descripción del tratamiento 1">
+      <p>Tratamientos de periodoncia para mejorar la salud
+        de las encías y tratar la gingivitis y periodontitis</p>
+      <a class="button-tratamiento" href="./Tratamientos.php">Periodoncia</a>
+    </div>
+    <div class="photo-tratamientos">
+      <img src="img\DOS.webp" alt="Descripción del tratamiento 2">
+      <p>Tratamientos dentales en niños para tratar y
+        prevenir cualquier problema bucodental y cuidar de su salud oral.</p>
+      <a class="button-tratamiento" href="./Tratamientos.php">Odontopediatra</a>
+    </div>
+    <div class="photo-tratamientos">
+      <img src="img\TRES.webp" alt="Descripción del tratamiento 3">
+      <p>Tratamientos de estética dental mediante
+        blanqueamiento, carillas, prótesis dentales y cirugía oral</p>
+      <a class="button-tratamiento" href="./Tratamientos.php">Estetica Dental</a>
+    </div>
+  </div>
+
+  <!-- Segundo conjunto de fotos -->
+  <div class="photo-container-tratamientos">
+    <div class="photo-tratamientos">
+      <img src="img\CUATRO.webp" alt="Descripción del tratamiento 4">
+      <p>Tratamientos de ortodoncia en adultos y niños con aparatos fijos o
+        removibles, brackets, invisalign y ortodoncia lingual.</p>
+      <a class="button-tratamiento" href="./Tratamientos.php">Ortodoncia</a>
+    </div>
+    <div class="photo-tratamientos">
+      <img src="img\CINCO.webp" alt="Descripción del tratamiento 5">
+      <p>Tratamientos de implantología dental para recuperar las
+        piezas perdidas y mantener la salud de los dientes.</p>
+      <a class="button-tratamiento" href="./Tratamientos.php">Implantes Dental</a>
+    </div>
+    <div class="photo-tratamientos">
+      <img src="img\SEIS.webp" alt="Descripción del tratamiento 6">
+      <p>Tratamientos de odontología general y conservadora desde
+        prevención hasta rehabilitación para asegurar una boca sana.</p>
+      <a class="button-tratamiento" href="./Tratamientos.php">Odontologia general</a>
+    </div>
+  </div>
+
+
+  <!-- SOMOS TU DENTISTA DE CONFIANZA -->
+
+  <div class="section-dentista">
+    <div class="left-content">
+      <div class="title">
+        <h2>Somos tu Dentista de confianza</h2>
+      </div>
+      <div class="image">
+        <img src="img/confianza.jpeg" alt="Imagen del Dentista" />
+      </div>
+    </div>
+    <div class="right-content">
+      <div class="description">
+        <p>Sentir miedo o respeto al dentista, a determinados tratamientos,
+          a ciertas situaciones que suponen una urgencia dental,… a veces es bastante común.</p>
+
+        <p>Para personas que experimentan este tipo de sensaciones, ir al dentista se convierte en
+          algo terrible. Pero, si este es tu caso, el miedo o la ansiedad no deben impedirte cuidar de tu salud o estética bucodental.</p>
+
+        <p>En Sevilla Dental todos miembros del equipo están capacitados para meterse en la piel de cada paciente y aliviar todos sus posibles
+          temores de la mejor manera. Convirtiendo así la experiencia, a priori negativa según la percepción
+          de algunos pacientes, en algo realmente positivo.</p>
+
+        <p>¡No dejes de ir al dentista por miedos infundados! Tu mente es la que ha creado esos miedos. De la misma forma que los ha creado,
+          también puede vencerlos. Requiere un poco de esfuerzo por tu parte,
+          pero no te preocupes porque nosotros te vamos a ayudar. Confía en nuestro
+          equipo Sevilla Dental, tu clínica dental en Sevilla que te entiende.</p>
+      </div>
+    </div>
+  </div>
+
+
+
+  <!--UBICACION -->
+  <div class="ubicacion-seccion">
+    <div class="contenido-contenedor">
+      <div id="mapa" class="mapa-contenedor">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13997.81522540898!2d-100.52275100000001!3d28.705981!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x865f8d3415d50c55%3A0x5ea64b5faff5b97c!2sEndohospital!5e0!3m2!1ses-419!2smx!4v1700374405438!5m2!1ses-419!2smx" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      <div class="info-contenedor">
+        <h2>Donde estamos</h2>
+        <p>Te ayudamos con tu salud y estética dental. Cuéntanos qué necesitas.</p>
+        <button id="boton-redireccion" class="boton-personalizado">UBICACION</button>
+      </div>
+    </div>
+  </div>
+
+  <script>
+    document.getElementById("boton-redireccion").onclick = function() {
+      window.location.href = "ubicacion.php"; // Redirecciona a la página "ubicacion.php"
+    };
+  </script>
+
+
+  <!--solcitar cita -->
+  <div class="urgencias-dentales-seccion">
+    <div class="contenido-contenedor">
+      <div class="descripcion-contenedor">
+        <h2>Servicio de Urgencias Dentales todos los días</h2>
+        <p>En la Clínica Sevilla Dental queremos ofrecer la máxima cobertura a nuestros pacientes. Por eso ponemos a tu disposición un servicio de Urgencias Dentales todos los días, fines de semana y festivos incluidos para aquellas situaciones en las que necesites tratar tu problema con la mayor rapidez posible.</p>
+        <button id="solicitar-cita" class="boton-urgencias">Solicitar una cita</button>
+      </div>
+    </div>
+  </div>
+
+
+
+</html>
+<?php include 'header/pie.php'; ?>
